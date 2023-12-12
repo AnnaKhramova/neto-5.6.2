52de4a9db87c04a08e09e258f61a689aa4da0d19
akhramova <xaa2128@mail.ru>
2023-12-12 12:59:01 +0300

    Задание 2

--- src/test/java/ru/akhramova/MainTest.java
+++ src/test/java/ru/akhramova/MainTest.java
@@ -1,11 +1,20 @@
 package ru.akhramova;
-
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.beans.HasPropertyWithValue.hasProperty;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.hamcrest.number.OrderingComparison.greaterThan;
+import static org.hamcrest.number.OrderingComparison.lessThan;
+import static org.hamcrest.Matchers.instanceOf;
 
 class MainTest {
 
@@ -22,7 +31,7 @@ class MainTest {
         //when
         String actual = Main.listToJson(list);
         //then
-        Assertions.assertEquals(expected, actual);
+        assertThat(expected, equalTo(actual));
     }
 
     @Test
@@ -33,7 +42,7 @@ class MainTest {
         //when
         String actual = Main.readString(fileName);
         //then
-        Assertions.assertEquals(expected, actual);
+        assertThat(expected, equalTo(actual));
     }
 
     @Test
@@ -43,9 +52,47 @@ class MainTest {
         //when
         List<Employee> actual = Main.jsonToList(json);
         //then
-        Assertions.assertNotNull(actual);
-        Assertions.assertEquals(actual.size(), 2);
-        Assertions.assertEquals(actual.get(0).age, 25);
-        Assertions.assertEquals(actual.get(1).firstName, "Ivan");
+        assertThat(actual, is(not(nullValue())));
+        assertThat(actual, is(not(empty())));
+        assertThat(actual.size(), equalTo(2));
+        assertThat(actual.get(0).age, equalTo(25));
+        assertThat(actual.get(1).firstName, equalTo("Ivan"));
     }
+
+    // Почему-то не работает :(
+    // Выдает ошибку
+    // java.lang.NoSuchMethodError: 'void org.hamcrest.Matcher.describeMismatch(java.lang.Object, org.hamcrest.Description)'
+//    @Test
+//    void hasPropertyTest() {
+//        //given
+//        String json = "[  {    \"id\": 1,    \"firstName\": \"John\",    \"lastName\": \"Smith\",    \"country\": \"USA\",    \"age\": 25  },  {    \"id\": 2,    \"firstName\": \"Ivan\",    \"lastName\": \"Petrov\",    \"country\": \"RU\",    \"age\": 23  }]";
+//        //when
+//        List<Employee> actual = Main.jsonToList(json);
+//        Employee employee = actual.get(0);
+//        //then
+//        assertThat(employee, hasProperty("lastName", equalTo("Smith")));
+//    }
+
+    @Test
+    void compareAgeTest() {
+        //given
+        String json = "[  {    \"id\": 1,    \"firstName\": \"John\",    \"lastName\": \"Smith\",    \"country\": \"USA\",    \"age\": 25  },  {    \"id\": 2,    \"firstName\": \"Ivan\",    \"lastName\": \"Petrov\",    \"country\": \"RU\",    \"age\": 23  }]";
+        //when
+        List<Employee> actual = Main.jsonToList(json);
+        //then
+        assertThat(actual.get(0).age, greaterThan(actual.get(1).age));
+        assertThat(actual.get(0).age, lessThan(30));
+    }
+
+    @Test
+    void instanceOfTest() {
+        //given
+        String json = "[  {    \"id\": 1,    \"firstName\": \"John\",    \"lastName\": \"Smith\",    \"country\": \"USA\",    \"age\": 25  },  {    \"id\": 2,    \"firstName\": \"Ivan\",    \"lastName\": \"Petrov\",    \"country\": \"RU\",    \"age\": 23  }]";
+        //when
+        List<Employee> actual = Main.jsonToList(json);
+        //then
+        assertThat(actual.get(0).lastName, is(instanceOf(String.class)));
+        assertThat(actual.get(1).country, is(instanceOf(String.class)));
+    }
+
 }
